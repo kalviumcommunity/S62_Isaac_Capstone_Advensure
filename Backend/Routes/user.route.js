efb1c51b0fb30673a09cbb8780dcf70a8282fa5c
Isaac Reji <isaac.reji@kalvium.community>
2025-02-25 10:36:09 +0530

    login signup and logout post endpoints done

--- Backend/Routes/user.route.js
+++ Backend/Routes/user.route.js
@@ -2,8 +2,11 @@ const express=require('express');
 const {createUser, loginUser, getUser, logoutUser}=require("../controllers/user.controller.js")
 const verifyUser=require("../Middlewares/jwt.verify.js")
 const router=express.Router()
+//POST Request
 router.post("/signup",createUser)
+//POST Request
 router.post("/login",loginUser)
 router.get('/get-user',verifyUser,getUser)
+//POST Request
 router.post('/logout',logoutUser)
 module.exports=router
